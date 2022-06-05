c65b8d4c21050f625e4dc910740f6948fa79489e
ThyIndie <thyindie@gmail.com>
2022-06-04 21:46:26 -0300

    Reestruturação do mapa do jogo e configuração da logica do game
    Este commit possui diversas alterações referentes ao mapa do game e sua lógica.

---
+++ SidiaGame/Assets/DevArea/GroundScripts/TilesManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SidiaGame.GroundScripts
+{
+    public class TilesManager : MonoBehaviour
+    {
+        [System.Serializable]
+        public class GameOptions
+        {
+            [Tooltip("All hexes that are part of the floor are on this list")]
+            public GameObject[] GroundTiles;
+
+            [Tooltip("Manually decide how many tiles will be of this color")]
+            [Range(1, 256)]
+            public float RedRange = 1;
+            [Tooltip("Manually decide how many tiles will be of this color")]
+            [Range(1, 256)]
+            public float GreenRange = 1;
+            [Tooltip("Manually decide how many tiles will be of this color")]
+            [Range(1, 256)]
+            public float YellowRange = 1;
+            [Tooltip("Manually decide how many tiles will be of this color")]
+            [Range(1, 256)]
+            public float BlueRange = 1;
+
+            [Tooltip("Check this box so that the computer decides the colors.")]
+            public bool Random;
+
+            [Tooltip("Spawn time between one block and another at the beginning of the game")]
+            [Range(0.01f, 1)]
+            public float SetSpeed = 0.05f;
+
+            public float ManagerColors() => RedRange + GreenRange + YellowRange + BlueRange;
+        }
+
+        public GameOptions GM;
+        public GameObject Last;
+
+        public List<int> Colors;
+        //1 - RED, 2 - Green, 3 - Yellow, 4- Blue
+        public List<Material> MaterialColor;
+        void Start()
+        {
+
+            GM.GroundTiles = GameObject.FindGameObjectsWithTag("Tile");
+
+            foreach (GameObject tiles in GM.GroundTiles)
+                tiles.SetActive(false);
+
+            ApplyRangeColors(true);
+
+
+        }
+
+        private void Update()
+        {
+
+
+        }
+
+        /// <summary>
+        /// Called when the game will start, this void defines the collectibles of each block
+        /// </summary>
+        /// <param name="IsRandom">True for a random generate tiles</param>
+        void ApplyRangeColors(bool IsRandom)
+        {
+
+            if (IsRandom)
+            {
+                for (int i = 0; i < GM.GroundTiles.Length; i++)
+                {
+                    Colors.Add(Random.Range(0, 4));
+                }
+
+
+                StartCoroutine(PaintGround(GM.SetSpeed, 0));
+            }
+            else
+            {
+
+
+            }
+        }
+
+        IEnumerator PaintGround(float T, int index)
+        {
+            yield return new WaitForSeconds(T);
+            if (index < GM.GroundTiles.Length)
+            {
+               
+                GM.GroundTiles[index].SetActive(true);
+                GM.GroundTiles[index].GetComponent<Renderer>().material = MaterialColor[Colors[index]];
+                StartCoroutine(PaintGround(GM.SetSpeed, index + 1));
+            }
+            else
+            {
+
+            }
+        }
+
+
+    }
+}
---
+++ SidiaGame/Assets/DevArea/PlayerScripts/CharacterManager.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SidiaGame.PlayerCode
+{
+    public class CharacterManager : MonoBehaviour
+    {
+        [System.Serializable]
+        public class AdminArea
+        {
+            [Range(1, 100)]
+            public float Health=1;
+            [Range(1, 100)]
+            public float Atack=1;
+
+            public Vector3 Offset = new Vector3(0,0.5f,0);
+        }
+        [Tooltip("Character settings control section")]
+        public AdminArea GM;
+
+        #region AtackArea
+        bool AtackOpen;
+
+        #endregion
+
+
+        #region InternalControllers
+        float HorizontalMove() => Input.GetAxisRaw("Horizontal");
+        float VerticalMove() => Input.GetAxisRaw("Vertical");
+
+        /// <summary>
+        /// In movement?
+        /// </summary>
+        bool inMove;
+
+        /// <summary>
+        /// Remaining moves for this turn
+        /// </summary>
+        int Move;
+
+        RaycastHit _right;
+        RaycastHit _left;
+        RaycastHit _forward;
+        RaycastHit _backward;
+        #endregion
+
+        private void Update()
+        {
+            ControllerMove();  
+        }
+
+        void ControllerMove()
+        {
+            if (!inMove)
+            {
+                if (HorizontalMove() != 0)
+                    StartMove(new Vector3(HorizontalMove(), 0, 0));
+                else if (VerticalMove() != 0)
+                    StartMove(new Vector3(0, 0, VerticalMove()));
+            }
+        }
+
+        void ControllerAtack()
+        {
+
+        }
+
+        /// <summary>
+        /// First step to move player
+        /// </summary>
+        /// <param name="Move">Direction of movement</param>
+        void StartMove(Vector3 _move)
+        {
+            if (Move > 0)
+            {
+                inMove = true;
+                bool Inuse = Physics.Raycast(transform.position + GM.Offset, _move, 1);
+                if (!Inuse)
+                {
+                    transform.position = transform.position + _move;
+                }
+                StartCoroutine(InMoviment());
+                Move--;
+            }
+        }
+        /// <summary>
+        /// Use to reset state of movement
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator InMoviment()
+        {
+            yield return new WaitForSeconds(0.1f);
+            inMove = false;
+            VerifyAtack();
+        }
+
+        /// <summary>
+        /// Checks if after the move it is possible to attack
+        /// </summary>
+        void VerifyAtack()
+        {
+            if (Physics.Raycast(transform.position + GM.Offset, transform.forward, out _forward, 1))
+                AtackOpen = true;
+            if (Physics.Raycast(transform.position + GM.Offset, -transform.forward, out _backward, 1))
+                AtackOpen = true;
+            if (Physics.Raycast(transform.position + GM.Offset, transform.right, out _right, 1))
+                AtackOpen = true;
+            if (Physics.Raycast(transform.position + GM.Offset, -transform.right, out _left, 1))
+                AtackOpen = true;
+        }
+
+        
+
+    }
+
+
+
+
+
+
+    
+
+    
+}
